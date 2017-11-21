58e8f403ee0f02320787aeee5802c50becc8ba69
Sarika Matthew <pragmattika@Sarika-Matthews-MacBook-Pro-2.local>
2017-11-21 10:19:24 -0500

    start javascript/jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+
+
+	//defaults for game
+
+	var randomNumber
+	var yourTotal = 0;
+	var winCount = 0;
+	var lossCount = 0;
+	var message = {
+		win: "YOU WIN!",
+		lose: "You collected too many crystals! GAME OVER!",
+	}
+
+
+	//set random numbers
+
+
+	//start a new game
+
+
+	//click crystals
+
+
+	//check for winner
+
+
+	//if win, add to win count
+
+
+	//if loss, add to loss count
+
+
+
+
+
+
+
+})
